f19e3d15ad62acfdbf9903364c06a51a887f9194
lauriekell <laurie@kell.es>
2018-08-14 13:28:33 +0100

    spwn=0

---
+++ tasks/task4/R/mlz.R
@@ -0,0 +1,45 @@
+
+source('~/Desktop/flr/FLife/R/omOut.R') 
+
+library(reshape)
+library(FLCore)
+library(ggplotFL)
+library(FLife)
+library(MLZ)
+
+load("/home/laurence/Desktop/sea++/mydas/tasks/task4/data/turbot.RData")
+
+ts=omSmry(om,eq,lh)
+
+
+mlzFn<-function(object,params){
+
+  dat=new("MLZ_data", 
+          Year      =unique(object$year), 
+          MeanLength=object$cln,
+          ss        =rep(500,length(object$year)),
+          length.units = "cm")
+  
+  dat@Lc    =c(params["lc"])
+  dat@vbLinf=c(params["linf"])
+  dat@vbK   =c(params["k"])
+  
+  hat=ML(dat,ncp=1)
+  
+  hat@estimates}
+
+object=subset(ts,year%in%50:60)
+res=mdply(sort(as.numeric(unique(ts$iter))), function(i,object,params){
+    res=mlzFn(subset(object,iter==i),params[,i])
+    cbind(var=c("z1","z2","year","sigma"),res)
+    },object=object,params=prior)
+
+
+ggplot(subset(res,!(var%in%c("sigma","year"))))+
+  geom_boxplot(aes(x=var,as.numeric(as.character(Estimate))),outlier.colour = NA)+
+  xlab("")+ylab("Z")+
+  scale_y_continuous(limits=c(0,1))
+
+ggplot(subset(res,(var%in%c("year"))))+
+  geom_histogram(aes(as.numeric(as.character(Estimate))))+
+  xlab("Year")+ylab("Count")
--- tasks/task4/R/mse/mse-sra.R
+++ tasks/task4/R/mse/mse-sra.R
@@ -14,7 +14,10 @@ mseStart=c("brill"=54,"turbot"=54,"ray"=60,"pollack"=56,"sprat"=52,"razor"=54,"l
 
 source('~/Desktop/flr/mpb/R/mseMPB.R')
 source("/home/laurence/Desktop/flr/mpb/R/hcr.R")
+source('~/Desktop/flr/mpb/R/mseSRA.R')
+source('~/Desktop/flr/mpb/R/biodyn-sra.R')
 source('~/Desktop/flr/FLife/R/omOut.R')
+
 
 theme_set(theme_bw())
 
@@ -36,15 +39,14 @@ mp=mpb:::setMP(as(window(om,end=55),"biodyn"),
 nits=dims(om)$iter
 set.seed(1234)
  srDev=FLife:::rlnoise(nits,FLQuant(0,dimnames=list(year=1:100)),0.3,b=0.0)
-  uDev=FLife:::rlnoise(nits,FLQuant(0,dimnames=list(year=1:100)),0.2,b=0.0)
-selDev=FLife:::rlnoise(nits,FLQuant(0,dimnames=list(year=1:100,age=dimnames(m(om))$age)),0.2,b=0.0)
 
 eq=FLCore:::iter(eq,seq(nits))
 
-mse2=mseMPB2(om,eq,mp,start=55,ftar=0.5,srDev=srDev,uDev=uDev,selDev=selDev)
+#mse=mseSRA(om,eq,mp,start=55,ftar=0.5,srDev=srDev)
 
 scen=expand.grid(stock=c("turbot","lobster","ray","pollack","razor","brill","sprat"),
-                  ftar=c(1.0,0.7),btrig=c(0.5,0.6))
+                  ftar =c(1.0,0.7),
+                  btrig=c(0.5,0.6))
 
 runMPB<-function(stock,ftar,btrig) {
   load(file.path(dirDat,paste(stock,".RData",sep="")))
@@ -64,20 +66,20 @@ runMPB<-function(stock,ftar,btrig) {
                  b0=0.8,
                  p =median(mpb:::p(prior["bmsy"]/prior["v"]),na.rm=TRUE))
   
-  res=mseMPB2(om,eq,mp,
+  res=mseSRA(om,eq,mp,
               start=mseStart[stock],end=mseStart[stock]+40,
               ftar=ftar,btrig=btrig,
-              srDev=srDev,uDev=uDev,selDev=selDev)
+              srDev=srDev)
   
   res2=data.frame("stock"=stock,"ftar"=ftar,"btrig"=btrig,omSmry(res,eq,lh[c("a","b")]))
   
   res2}
 
-mpb=NULL
+srampb=NULL
 for (i in seq(dim(scen)[1])){
   
   res=with(scen[i,],runMPB(stock,ftar,btrig))
   
-  mpb=rbind(mpb,cbind(scen=i,res))
+  srampb=rbind(srampb,cbind(scen=i,res))
   
-  save(mpb,file="/home/laurence/Desktop/sea++/mydas/tasks/task5/data/mpb.RData")}
+  save(srampb,file="/home/laurence/Desktop/sea++/mydas/tasks/task5/data/srampb.RData")}
