0a80b95e45534be6f5c835705270f3cab93f1eb3
lauriekell <laurie@kell.es>
2018-09-07 12:06:34 +0100

    debug

--- pkgs/mydas/R/hcrSBTD.R
+++ pkgs/mydas/R/hcrSBTD.R
@@ -2,19 +2,24 @@ hcrSBTD<-function(yrs,
                   control=c(k1=2.0,k2=3.0,gamma=1),
                   index,
                   catch,...){
-  
+print(1)  
   lambda=as.FLQuant(ddply(as.data.frame(index%/%apply(index,6,mean)), 
                           .(iter), with,  data.frame(data=coefficients(lm(data~year))[2])))
-  
+print(2)  
   flag  =lambda<0
   lambda=abs(lambda)
+print(3)  
   res   =1+ifelse(flag,-control["k1"],control["k2"])*exp(log(lambda)*ifelse(flag,control["gamma"],1))
+print(4)  
   res   =res%*%apply(catch,6,mean)
+print(5)  
   
   dmns     =dimnames(catch)
   dmns$year=yrs
   dmns$iter=dimnames(index)$iter
+print(6)  
   
   res      =FLQuant(rep(c(res),each=length(yrs)),dimnames=dmns)
+print(7)  
   
   return(res)}
